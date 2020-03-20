bd2536a54ac19f95b7ea9bb200e9f633276cc38b
Damely Tineo <damely.tineo07@gmail.com>
2020-03-20 04:24:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,11 +7,9 @@ class App < Sinatra::Base
     erb :index
   end
   
-  
   get '/new' do
     erb :create_puppy
   end
-  
   
   post '/puppy' do
     @puppy = Puppy.new(params["name"], params["breed"], params["age"])
