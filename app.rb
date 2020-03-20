e1da779c0cfcc1aeace26bb58e16dbe3d747f4ff
Damely Tineo <damely.tineo07@gmail.com>
2020-03-20 03:35:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,19 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do
-    erb :index
-  end
-  
   get '/new' do
     erb :index
   end
+  
   
   post '/puppy' do
     erb :create_puppy
   end
   
-  
+    @puppy = 
     erb :display_puppy
   end
 end
